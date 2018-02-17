c4bc69b5d3c3e3e0b8da49c77fba4c43d2b67f73
benyamerican <35763971+benyamerican@users.noreply.github.com>
2018-02-17 16:37:37 -0700

    well, little more update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,128 +1,3 @@
-// Choose a theme for your game! In the demo, we picked an 80s theme: 80s questions, 80s sound and an 80s aesthetic. You can choose any subject for your theme, though, so be creative!
-// Use key events to listen for the letters that your players will type.
-// Display the following on the page:
-// Press any key to get started!
-// Wins: (# of times user guessed the word correctly).
-
-
-
-// If the word is madonna, display it like this when the game starts: _ _ _ _ _ _ _.
-// As the user guesses the correct letters, reveal them: m a d o _  _ a.
-
-
-
-// Number of Guesses Remaining: (# of guesses remaining for the user).
-// Letters Already Guessed: (Letters the user has guessed, displayed like L Z Y H).
-// After the user wins/loses the game should automatically choose another word and make the user play it.
-
-
-
-// __________________________________________________________________
-
-
-// Hangman Game Bonuses
-
-
-// Play a sound or song when the user guesses their word correctly, like in our demo.
-// Write some stylish CSS rules to make a design that fits your game's theme.
-
-// HARD MODE: Organize your game code as an object, except for the key events to get the letter guessed. This will be a challenge if you haven't coded with JavaScript before, but we encourage anyone already familiar with the language to try this out.
-// Save your whole game and its properties in an object.
-// Save any of your game's functions as methods, and call them underneath your object declaration using event listeners.
-// Don't forget to place your global variables and functions above your object.
-
-
-// Remember: global variables, then objects, then calls.
-
-
-// Definitely talk with a TA or your instructor if you get tripped up during this challenge.
-
-
-
-// _______________________________________________________________________________________
-
-
-
-
-
-// `1. Declare all possible words with an array.
-
-
-
-
-
-
-
-// 2. Write a function that generates a random number based off the length of your array. Use that number to get a value from your array. That value is your random word.
-
-
-
-
-// 3. We need to have each letter of the word written to our html document. Think of a way to split up your random word, and create a container for each word to write to your html document.
-
-
-
-
-
-
-// 4. We need an event listener on our keyboard to check if the letter we pressed is contained in our random word.
-
-
-
-
-
-// 5. If the key pressed is contained in our random word, then we need to reveal the hidden letter. If the key pressed is not contained in our random word, then we need to subtract from our total tries. First make sure that you can reveal a correct guess before trying to count how many mistakes have been made. This will be easier.
-
-
-
-
-// 6. Display a list of correct guesses. If the word contains an A, display on the screen that an was used, and was correct. Make it so that the same letter is not used twice.
-
-
-
-
-
-// 7. Find a way to check if the game is over. This includes checking if all the letters have been revealed, which will be a win, and checking if the total tries has been used, which will be a loss.
-
-
-
-
-// 8. Track the total number of wins and losses.
-
-
-// Do some research on the following. Must all be done with JavaScript, no jQuery:
-
-// select an element by ID,
-// create a new element,
-// append an element,
-// select all children elements,
-// remove all children elements,
-// add a class or id to an element,
-// remove a class or id on an element,
-// select all elements with the same class.
-
-
-// All of these will be useful when you are showing the hidden word, revealing the letters, and changing the word, as well as updating the wins and losses.
-
-
-
-
-// 1-The game will have some wordlist to selct word from
-// 2-the game will only selct words from wordlist
-//3-there will be on screen image persenting any word
-//4-All guessed letters will be display on screen
-//5-The player doesnt lose by making same mistake
-//6-only alphabets are choosable
-
-//1.1 selct the word randomly the show blanks on screen
-
-//2.2guess letter -review the guess-add guess-update
-
-//3.3 see win-lose statuse-review live left-confirm win/lose
-
-
-
-
 // _____________________________START___________________________//
 
 
@@ -154,7 +29,7 @@ messages = {
     validLetter: 'Please enter a letter from A-Z'
 };
 var lives = 6;
-var x ="";
+
 
 
 
@@ -172,28 +47,81 @@ let randomLegend = legends[Math.floor(Math.random() * legends.length)].toUpperCa
 // _______________________________________split The random Pick_________________________
 
 
-let lettersOfLegends = randomLegend.split("");
+var lettersOfLegends = randomLegend.split("");
  
- console.log(lettersOfLegends);
+//  console.log(lettersOfLegends);
    
 
 //  ________________________for loop for letters of each legend_________________________________
 
-for (i = 0; i < lettersOfLegends.length; i++){
-    console.log(lettersOfLegends[i]);
+// for (i = 0; i < lettersOfLegends.length; i++){
+//     console.log(lettersOfLegends[i])};
+// 
+
+// __Generate  start key and Call a legend _show blank as element length___make hint button availabe______
+// ______________________________________________
+
+const userStartsTheGame = document.getElementById("PressingKey");
+
+var answerArray = [];
+ for (var i = 0; i < randomLegend.length; i++) {
+    answerArray[i] = " _ ";
+ 
 }
-// __________________________________Generate key input_________________________
 
 
-function myFunction(event) {
 
-    document.getElementById("demo").innerHTML = "The pressed key was";
-    
-
-   
-   
-   
+document.addEventListener("keyup", startGame);function startGame(){
+    for (i = 0; i < lettersOfLegends.length; i++){
+        userStartsTheGame.textContent = "Guess the Legend "  +
+         answerArray;  
 }
+};
+
+
+
+// _____________________________________ guessing The letters__________________________________________________
+
+
+document.onkeyup = function guessedLetters(){
+
+    var userGuess = event.key;
+    // console.log(userGuess);
+    // __________________________________right or wrong key pressed alert__________________________________
+
+var keyPressed = event.keyCode; 
+if (keyPressed >= 65 && keyPressed <= 122 ) {
+
+    console.log("keep Going");
+}
+else {
+    console.log("What are you doing? Choose a letter idiot!");
+}
+}
+  
+//    ______________________________________End Of Guessing Letters Function______________________
+
+
+
+// ___________________________________________REMOVE CHOOsEN ELEMENT________________
+
+
+
+
+// _____________________________________
+
+
+
+// ___________________________________Pussh guess characters in____________
+// ___________________________count down the lives_______________________
+
+// _______________________If all guesses are corect add one to wins and move to the next legend___________
+
+
+//________________________________________if lives goes to zero add one to loses_____________
+
+
+
 
 
 
